d83a4e5e453554709688a6406a125f23d01278aa
Pedro Andrés Chaparro Quintero <pedro.chaparro.2020@upb.edu.co>
2023-02-13 16:41:47 -0500

    :construction: Create routes
    Create home and login routes

--- 03-Login/frontend/lib/main.dart
+++ 03-Login/frontend/lib/main.dart
@@ -1,6 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:session/screens/home_screen.dart';
 import 'package:session/screens/login.dart';
 
 void main() {
-  runApp(const LoginScreen());
+  runApp(MaterialApp(
+    title: 'Session',
+    initialRoute: '/login',
+    routes: {
+      '/login': (context) => const LoginScreen(),
+      '/home': (context) => const HomeScreen(),
+    },
+  ));
 }
